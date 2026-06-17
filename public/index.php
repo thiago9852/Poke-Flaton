b5237271e19475fd2603567744169120309b2940
Thiago ferreira <thiagoferreira.113@gmail.com>
2026-06-17 11:20:10 -0300

    Fix: Login

--- public/index.php
+++ public/index.php
@@ -1,9 +1,18 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return static function (array $context) {
+    $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+    if ($trustedProxies) {
+        Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_HOST);
+    } elseif (($context['APP_ENV'] ?? 'dev') === 'prod') {
+        // Automatically trust the current request's remote address if running in production behind a load balancer
+        Request::setTrustedProxies(['127.0.0.1', '::1', $_SERVER['REMOTE_ADDR'] ?? ''], Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_HOST);
+    }
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
