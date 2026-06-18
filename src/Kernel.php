25e074df06ab87cc584f24e0f69a3d07e56209bb
Thiago ferreira <thiagoferreira.113@gmail.com>
2026-06-18 12:43:22 -0300

    Time-zone

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,11 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function boot(): void
+    {
+        parent::boot();
+        $timezone = $_ENV['APP_TIMEZONE'] ?? $_SERVER['APP_TIMEZONE'] ?? 'America/Sao_Paulo';
+        date_default_timezone_set($timezone);
+    }
 }
